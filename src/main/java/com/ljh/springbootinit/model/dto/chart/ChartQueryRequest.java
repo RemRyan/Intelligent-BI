bef7a66e083baf5c86701a1f478479c07b7b5fa4
RemRyan <67848547@qq.com>
2023-12-22 12:27:17 +0800

    init 第一次初始化

---
+++ src/main/java/com/ljh/springbootinit/model/dto/chart/ChartQueryRequest.java
@@ -0,0 +1,49 @@
+package com.ljh.springbootinit.model.dto.chart;
+
+import com.ljh.springbootinit.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+/**
+ * 查询请求
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class ChartQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 图表名称
+     */
+    private String name;
+
+    /**
+     * 分析目标
+     */
+    private String goal;
+
+    /**
+     * 图表类型
+     */
+    private String chartType;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    /**
+     * 图表标签 Json 列表
+     */
+    private String tags;
+
+    private String status;
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/ljh/springbootinit/model/vo/UserVO.java
@@ -0,0 +1,54 @@
+package com.ljh.springbootinit.model.vo;
+
+import java.io.Serializable;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ * 用户视图（脱敏）
+ */
+@Data
+public class UserVO implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户昵称
+     */
+    private String userName;
+
+    /**
+     * 用户头像
+     */
+    private String userAvatar;
+
+    /**
+     * 用户简介
+     */
+    private String userProfile;
+
+    /**
+     * 用户积分余额 默认注册赠送30积分
+     */
+    private Long balance;
+
+    /**
+     * 用户角色：user/admin/ban
+     */
+    private String userRole;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+}
